cc82b340996edbea066e9f0f8bd8a04733c6cbd1
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-01-20 18:18:09 +0100

    Update invite.py

--- cogs/invite.py
+++ cogs/invite.py
@@ -13,8 +13,6 @@ class invite(commands.Cog):
                 color = discord.Colour.from_rgb(239, 164, 176)
             )
             embed.add_field(name='Currently not available!', value='Sorry..')
-            #embed.add_field(name="Use the link to invite the bot to your server!", value='*[Invite](https://discord.com/api/oauth2/authorize?client_id=743150068726628440&permissions=8&scope=bot/ "Do not worry, this is just an invitation!")*')
-            #embed.set_footer(text="I would be glad, if you'd add my bot to your server :>")
             await ctx.send(embed=embed)
     
     @commands.command()
