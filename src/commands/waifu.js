cfe6332dc065da59e6e86602705a1c04ba35a510
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-08-29 21:23:51 +0200

    waifu command stuff

--- src/commands/waifu.js
+++ src/commands/waifu.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { color } = require('../data/config.json');
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,12 +14,28 @@ module.exports = {
                 .addChoice('One', '1')
                 .addChoice('Five', '5')
                 .addChoice('Ten', '10')),
-    async execute(interaction) {
-        const amount = interaction.options.getString('amount');
-        const embed = new MessageEmbed()
-            .setColor(color)
-            .setTitle('Random waifu pic?')
-            .setImage('https://i.waifu.pics/3x~ovC6.jpg')
-        await interaction.reply({ embeds: [embed] });
+        
+        async execute(interaction) {
+            const amount = interaction.options.getString('amount');
+
+            async function fetchImage() {
+                let response = await fetch('https://api.waifu.pics/sfw/waifu');
+                let data = await response.json();
+                let img_url = data.url
+                return img_url;
+            }
+
+            var real_amount = amount-1;
+            const embed = new MessageEmbed()
+                .setColor(color)
+                .setTitle('Random waifu pic?')
+                .setImage(await fetchImage())
+        
+            
+            await interaction.reply({ embeds: [embed] });
+        for(var i=0; i < real_amount; i++){
+            embed.setImage(await fetchImage())
+            await interaction.followUp({ embeds: [embed]} );
+        }
     },
 };
