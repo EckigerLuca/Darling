948ba7f53e0db6f7b275652591619087871f945a
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-09-04 23:00:31 +0200

    Cat command

---
+++ src/commands/media/cat.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { color } = require('../../data/config.json');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch')
+
+module.exports = {
+    data: new SlashCommandBuilder() 
+        .setName('cat')
+        .setDescription('Returns a random cat')
+        .addStringOption(option =>
+            option.setName('amount')
+                .setDescription('Defines how many images the bot should send')
+                .setRequired(true)
+                .addChoice('One', '1')
+                .addChoice('Five', '5')
+                .addChoice('Ten', '10')),
+        
+        async execute(interaction) {
+            const amount = interaction.options.getString('amount');
+
+            async function fetchImage() {
+                let response = await fetch('http://aws.random.cat/meow');
+                let data = await response.json();
+                let img_url = data.file
+                return img_url;
+            }
+
+            var real_amount = amount-1;
+            let img = await fetchImage()
+            const embed = new MessageEmbed()
+                .setColor(color)
+                .setTitle('Meow!')
+                .setDescription(`[Link if you can't see the image](${img})`)
+                .setFooter('From random.cat')
+                .setImage(img)
+        
+            
+            await interaction.reply({ embeds: [embed] });
+            for(var i=0; i < real_amount; i++){
+                embed.setImage(await fetchImage())
+                await interaction.followUp({ embeds: [embed]} );
+        }
+    },
+};
