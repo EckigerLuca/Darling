5f0b964ae2cb4d446f72797e1c8d1e9db48b24ce
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-09-05 21:12:25 +0200

    Added adidas command

---
+++ src/commands/meme/adidas.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('adidas')
+        .setDescription('adidas.mp4'),
+
+        async execute(interaction) {
+            const adidas = 'https://cdn.discordapp.com/attachments/743161085246570638/785943266544713738/Adidas.mp4';
+            await interaction.reply(`**Adidas.mp4**\n${adidas}`);
+    },
+};
