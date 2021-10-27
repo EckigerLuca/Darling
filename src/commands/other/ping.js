3dd3f1f0e890458df0725d9242e2c3237cbcdf9f
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-10-27 18:19:56 +0200

    Update ping command

--- src/commands/other/ping.js
+++ src/commands/other/ping.js
@@ -12,7 +12,7 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setColor(color)
                 .setTitle('Pong!')
-                .setDescription(`That took ${Date.now() - interaction.createdTimestamp}ms.\n\nAPI Latency is ${Math.round(interaction.client.ws.ping)}ms`)
+                .setDescription(`That took ${Math.abs(Date.now() - interaction.createdTimestamp)}ms.\n\nAPI Latency is ${Math.round(interaction.client.ws.ping)}ms`)
                 
             await interaction.reply({ embeds: [embed] });
     },
