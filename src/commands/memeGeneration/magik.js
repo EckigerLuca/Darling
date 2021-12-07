a9f20f191040ae5d119eb9e42e474b07e872287f
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-12-07 21:04:48 +0100

    magik command

---
+++ src/commands/memeGeneration/magik.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { color } = require('../../data/config.json');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('magik')
+        .setDescription('create magik')
+        .addUserOption(option => option.setName('target').setDescription('user to magik').setRequired(false)),
+    
+    async execute(interaction) {
+        await interaction.deferReply();
+        let target = interaction.options.getUser('target');
+        if (target === null) {
+            target = interaction.user
+        }
+        let avatar = target.displayAvatarURL({size: 512, format: 'png', dynamic: true})
+        let response = await fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}`)
+        const data = await response.json()
+
+        const embed = new MessageEmbed()
+            .setTitle('Magik')
+            .setColor(color)
+            .setImage(data.message)
+        await interaction.editReply({embeds: [embed]})
+    }
+};
