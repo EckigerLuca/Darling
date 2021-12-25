794f0f2d764af33535f0d5c850476bd66e01a545
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-12-25 22:31:30 +0100

    fuck command

---
+++ src/commands/roleplay/fuck.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { color } = require('../../data/config.json');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('fuck')
+        .setDescription('bang someone really hard')
+        .addUserOption(option => option.setName('target').setDescription('the person you want to bang').setRequired(true)),
+    
+    async execute(interaction) {
+        let user = interaction.options.getUser('target');
+
+        let sfwGif = 'https://eckigerluca.com/darling/media/fuck/fuck.gif'
+
+        if (interaction.channel.nsfw) {
+            let response = await fetch('http://eckigerluca.com:727/fuck');
+            let data = await response.json();
+
+            let embed = new MessageEmbed()
+                .setDescription(`${interaction.user} bangs the shit out of ${user}`)
+                .setColor(color)
+                .setImage(data.image)
+            await interaction.reply({embeds: [embed]})
+        } else {
+            const embed = new MessageEmbed()
+                .setDescription(`${interaction.user} is doing lewd things to ${user}`)
+                .setColor(color)
+                .setImage(sfwGif)
+            await interaction.reply({embeds: [embed]})
+        }
+    }
+};
