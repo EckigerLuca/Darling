f97574e70fab9be851a86c5f3d55e7493f959417
Luca <63116530+EckigerLuca@users.noreply.github.com>
2022-01-10 21:25:57 +0100

    added content

--- src/commands/other/botinfo.js
+++ src/commands/other/botinfo.js
@@ -20,12 +20,12 @@ module.exports = {
             .addFields(
                 {name: 'How it started...', value: "I actually decided to code my own bot because I felt like 'Ugh pay2win I dont like that' sooooo I decided to do it free then. Yes, this bot is completely free, but I have to pay to maintain it tho lol\nThe first few lines of code of `Darling.` were written in Python with discord.py, but sadly this library isn't maintained anymore so I had to switch to `Discord.js` which is obv written in JavaScript. Uh yeah, a date..  it was the 12th august 2020 when I started this project. How time has passed\nEnough bs, time for important information!", inline: false},
                 {name: "Profile Picture", value: `__Artist:__\n[ShiChi](https://www.pixiv.net/en/users/36083362)\n__Artwork:__\n[スカジ](https://www.pixiv.net/en/artworks/85612659)`, inline: true},
-                {name: "Servers:", value: `${servers}`, inline: false},
-                {name: "Code", value: "https://eckigerluca.com/darling/github"},
-                
+                {name: "Servers:", value: `${servers}`, inline: true},
+                {name: "Code", value: "https://eckigerluca.com/darling/github", inline: true},
+                {name: "Support", value: "There is the **Bot Support** on my [Server](https://eckigerluca.com/discord).\nBut you can also support **me** on [Patreon](https://patreon.com/eckigerluca) or [Ko-Fi](https://ko-fi.com/eckigerluca)", inline: false},
             )
-            .setFooter("Ich mag Bananensaft", eckigerluca_avatar)
-            .setAuthor("EckigerLuca", eckigerluca_avatar, "https://github.com/EckigerLuca/")
+            .setFooter({ text: "Ich mag Bananensaft", iconURL: eckigerluca_avatar})
+            .setAuthor({name: "EckigerLuca", iconURL: eckigerluca_avatar, url: "https://github.com/EckigerLuca/"})
         await interaction.reply({embeds: [embed]})
     }
 };
